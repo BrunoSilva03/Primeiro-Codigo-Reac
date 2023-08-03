cc59294a1d8bed3726eca86d7cace7efa1e16dd7
Bruno Silva Rodrigues <78625466+BrunoSilva03@users.noreply.github.com>
2023-08-03 11:54:59 -0300

    Add files via upload

---
+++ components/HelloWorld.jsx
@@ -0,0 +1,14 @@
+import Frase from './Frase'
+
+function HelloWorld() {
+
+    return (
+        <div>
+            <Frase />
+            <h1>Hello, World!</h1>
+            <h1>Meu primeiro componente React</h1>
+        </div>
+    )
+}
+
+export default HelloWorld
